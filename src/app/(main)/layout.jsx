c94010efd5be65d5f9ac3477a1dee74c2b49dafb
umayer10-AI <mdumayer10@gmail.com>
2026-04-25 14:14:31 +0600

    add

--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -1,9 +1,13 @@
+import BreakingNews from '@/component/BreakingNews';
+import Headers from '@/component/Headers';
 import Navbar from '@/component/Navbar';
 import React from 'react';
 
 const layout = ({children}) => {
     return (
         <div>
+            <Headers></Headers>
+            <BreakingNews></BreakingNews>
             <Navbar></Navbar>
             {children}
         </div>
