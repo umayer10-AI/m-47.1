64ca70b7a29525aa92156b0a32dd222db36d56bd
umayer10-AI <mdumayer10@gmail.com>
2026-04-25 14:28:34 +0600

    add

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,12 +1,13 @@
 import Category from '@/component/Category';
+import RightSide from '@/component/RightSide';
 import React from 'react';
 
 const page = () => {
   return (
-    <div>
-        <h2 className='text-3xl font-bold text-center my-5'>Home Page</h2>
+    <div className='grid grid-cols-3 max-w-[80%] mx-auto'>
+        {/* <h2 className='text-3xl font-bold text-center my-5'>Home Page</h2> */}
         <Category></Category>
-        
+        <RightSide></RightSide>
     </div>
   );
 };
