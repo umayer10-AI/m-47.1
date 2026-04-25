24e3bd63ea2601758b638a3bcc57d3fb8634194a
umayer10-AI <mdumayer10@gmail.com>
2026-04-25 13:57:33 +0600

    add

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import NavLink from './NavLink';
+
+const Navbar = () => {
+    return (
+        <div>
+            <div>
+
+            </div>
+            <NavLink></NavLink>
+            <div>
+                <h2><FaCircleUser /></h2>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
