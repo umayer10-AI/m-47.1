dbaa1c8da708e8878a52de856719f9032f7972f1
umayer10-AI <mdumayer10@gmail.com>
2026-04-26 08:27:59 +0600

    add

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,20 +1,33 @@
+"use client"
 import Navbar from '@/component/Navbar';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const page = () => {
+
+    const {register,handleSubmit,watch,formState: { errors }} = useForm()
+
+    const handleClick = (v) => {
+        console.log(v)
+    }
+
+    // console.log(watch("password"))
+
     return (
         <div>
             <Navbar></Navbar>
             <div className='flex justify-center mt-5 mb-10'>
-                <form className="fieldset bg-base-200 border-base-300 rounded-box w-md border p-8">
+                <form onSubmit={handleSubmit(handleClick)} className="fieldset bg-base-200 border-base-300 rounded-box w-md border p-8">
 
                     <h2 className='text-3xl font-semibold text-center mb-5'>Register your account</h2>
 
                     <label className="label text-black font-semibold text-base">Email address</label>
-                    <input type="email" className="input w-full" placeholder="Enter email address" />
+                    <input type="email" className="input w-full" placeholder="Enter email address"  {...register("email", { required: "Email filed required" })} />
+                    {errors.email && <p className='font-semibold text-red-500'>{errors.email.message}</p>}
 
                     <label className="label text-black font-semibold text-base">Password</label>
-                    <input type="password" className="input w-full" placeholder="Enter your password" />
+                    <input type="password" className="input w-full" placeholder="Enter your password"  {...register("password", { required: "password filed required" })} />
+                    {errors.password && <p className='font-semibold text-red-500'>{errors.password.message}</p>}
 
                     <button className="btn btn-neutral my-4">Login</button>
 
